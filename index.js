a4bee4f262a5a12d095a320157ba90c366976356
ChukwukaOnwana <onwanaokem@gmail.com>
2020-03-25 22:35:10 +0100

    My First Javascript Task

---
+++ index.js
@@ -0,0 +1,9 @@
+const myName = 'Okem';
+const myCourses = ['HTML','CSS','JAVASCRIPT','NODE.JS','UI/UX DESIGN']
+
+console.log(myName)
+console.log(myCourses)
+
+for(let i = 1;i <=200;i++){
+    if (i % 2 !== 0) console.log(i)
+}
